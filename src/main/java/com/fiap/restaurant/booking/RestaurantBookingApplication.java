6259467ba47dcd8461c53e7187ffd92ce560f64a
vinnilmg <vini.delimagomes@gmail.com>
2024-09-03 23:13:03 -0300

    feat: init project

---
+++ src/main/java/com/fiap/restaurant/booking/RestaurantBookingApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurant.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/restaurant/booking/RestaurantBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurant.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
