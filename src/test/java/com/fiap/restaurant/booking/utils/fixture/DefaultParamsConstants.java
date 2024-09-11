a683ac596e97c68dd839ccecd4cd4919c550d6a4
vinnilmg <vini.delimagomes@gmail.com>
2024-09-10 21:07:58 -0300

    test: Inicio dos testes unitarios

---
+++ src/test/java/com/fiap/restaurant/booking/utils/fixture/DefaultParamsConstants.java
@@ -0,0 +1,8 @@
+package com.fiap.restaurant.booking.utils.fixture;
+
+public class DefaultParamsConstants {
+    private DefaultParamsConstants() {
+    }
+
+    public static final String DEFAULT_CPF = "83533067009";
+}
