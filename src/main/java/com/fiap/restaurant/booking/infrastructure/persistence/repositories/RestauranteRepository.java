e18daca3ffb2a17d4c7ab9dcdfa3b07d03010271
vinnilmg <vini.delimagomes@gmail.com>
2024-09-15 18:34:46 -0300

    fix: Corrigindo repository de restaurante

--- src/main/java/com/fiap/restaurant/booking/infrastructure/persistence/repositories/RestauranteRepository.java
+++ src/main/java/com/fiap/restaurant/booking/infrastructure/persistence/repositories/RestauranteRepository.java
@@ -2,13 +2,13 @@ package com.fiap.restaurant.booking.infrastructure.persistence.repositories;
 
 import com.fiap.restaurant.booking.infrastructure.persistence.entities.RestauranteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface RestauranteRepository extends JpaRepository<RestauranteRepository, Long> {
+public interface RestauranteRepository extends JpaRepository<RestauranteEntity, Long> {
     List<RestauranteEntity> findByNome(String nome);
 
     List<RestauranteEntity> findByTipoCulinaria(String tipoCulinaria);
 
     List<RestauranteEntity> findByMediaFeedback(Double mediaFeedback);
-
 }
